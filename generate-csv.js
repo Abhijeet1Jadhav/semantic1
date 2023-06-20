92a6c177a2844b152a9299a1a55acc0d8148aba4
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2023-06-20 17:43:11 +0530

    Update generate-csv.js

--- generate-csv.js
+++ generate-csv.js
@@ -4,8 +4,8 @@ const { Octokit } = require('@octokit/rest');
 const octokit = new Octokit();
 
 async function generateCSV() {
-  const owner = 'your-username';
-  const repo = 'your-repository';
+  const owner = 'Abhijeet1Jadhav';
+  const repo = 'https://github.com/Abhijeet1Jadhav/semantic1.git';
 
   const { data: pullRequests } = await octokit.pulls.list({
     owner,
