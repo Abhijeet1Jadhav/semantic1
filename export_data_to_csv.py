58856aa84dde78b426f0bf22266a1a12d1fa0d41
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2023-06-26 14:58:30 +0530

    Update and rename report.cjs to export_data_to_csv.py

---
+++ export_data_to_csv.py
@@ -0,0 +1,38 @@
+import os
+import csv
+from github import Github
+
+# Get the GitHub token
+token = os.environ['GITHUB_TOKEN']
+
+# Create a GitHub instance
+g = Github(token)
+
+# Get the repository and pull request details
+repo = g.get_repo('username/repo')  # Replace with your repository details
+pulls = repo.get_pulls(state='open')  # Modify the pull request state as needed
+
+# Prepare the data for CSV
+data = [['Pull Request Number', 'Pull Request Title', 'Deployment Status', 'Workflow Step Status']]
+
+for pull in pulls:
+    # Get the deployment status for the pull request
+    # Modify this logic to retrieve the correct deployment status based on your requirements
+    deployment_status = 'Get deployment status here'
+
+    # Get the workflow runs for the pull request
+    workflow_runs = pull.get_check_runs()
+
+    # Loop through each workflow run and get the step status
+    for run in workflow_runs:
+        # Modify this logic to retrieve the correct step status based on your requirements
+        step_status = 'Get step status here'
+
+        # Append the data to the list
+        data.append([pull.number, pull.title, deployment_status, step_status])
+
+# Export data to CSV
+csv_file_name = sys.argv[1]
+with open(csv_file_name, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(data)
