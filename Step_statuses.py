c3029197310e6dcf4755bf60bd6292a6ecf5a461
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2023-06-29 17:22:22 +0530

    Update Step_statuses.py

--- Step_statuses.py
+++ Step_statuses.py
@@ -3,7 +3,7 @@ import requests
 import os
 
 # Replace with your personal access token
-ACCESS_TOKEN = ${{ secrets.RELEASE_GIT_TOKEN }}
+ACCESS_TOKEN = '${{ secrets.RELEASE_GIT_TOKEN }}'
 
 # Replace with your repository details
 REPO_OWNER = 'Abhijeet1Jadhav'
