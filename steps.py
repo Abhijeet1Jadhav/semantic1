32e6381c54bea4fe3be5637330a3d203201471b7
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2023-07-07 17:05:59 +0530

    Update steps.py

--- steps.py
+++ steps.py
@@ -3,7 +3,7 @@ import requests
 import os
 
 # Replace with your personal access token
-ACCESS_TOKEN = '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'
+ACCESS_TOKEN = 'os.environ['ACCESS_TOKEN']'
 
 # Replace with your repository details
 REPO_OWNER = 'Abhijeet1Jadhav'
