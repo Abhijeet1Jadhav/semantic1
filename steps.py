8bbc86fc4ddf11f6b6832c7933a77322410d4ed6
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2023-07-10 17:36:38 +0530

    Update steps.py

--- steps.py
+++ steps.py
@@ -9,7 +9,7 @@ ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 REPO_OWNER = 'Abhijeet1Jadhav'
 REPO_NAME = 'semantic1'
 #WORKFLOW_FILE = 'steps.yml'
-WORKFLOW_FILE = sys.argv[1]
+WORKFLOW_FILE = sys.argv[4]
 
 # Set the headers including the access token
 headers = {
