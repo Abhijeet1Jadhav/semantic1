f59f26024f41bc200d90f00b46e55557fca085de
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2023-07-17 17:15:05 +0530

    Update final.py

--- final.py
+++ final.py
@@ -22,8 +22,8 @@ def fetch_run_and_job_steps(run_id):
     # ... (existing code for fetching run and job steps)
 
 # Make a GET request to retrieve workflow runs
-api_url = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/actions/workflows/{WORKFLOW_FILE}/runs'
-response = requests.get(api_url, headers=headers)
+    api_url = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/actions/workflows/{WORKFLOW_FILE}/runs'
+    response = requests.get(api_url, headers=headers)
 
 # Check if the request was successful
 if response.status_code == 200:
