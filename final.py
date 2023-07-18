1acf3c23031bcbf0084657b0ade5955be1d86f92
Abhijeet1Jadhav <60206377+Abhijeet1Jadhav@users.noreply.github.com>
2023-07-18 16:36:19 +0530

    Update final.py

--- final.py
+++ final.py
@@ -5,9 +5,15 @@ import sys
 import requests
 
 # Get the job start time and end time as input parameters
-WORKFLOW_FILE = sys.argv[1]
-job_start_time = os.getenv('JOB_START_TIME')
-job_end_time = os.getenv('JOB_END_TIME')
+#WORKFLOW_FILE = sys.argv[1]
+#job_start_time = os.getenv('JOB_START_TIME')
+#job_end_time = os.getenv('JOB_END_TIME')
+
+#repo_owner = sys.argv[1]
+#repo_name = sys.argv[2]
+workflow_file = sys.argv[3]
+job_start_time = sys.argv[4]
+job_end_time = sys.argv[5]
 
 # Replace with your personal access token
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
